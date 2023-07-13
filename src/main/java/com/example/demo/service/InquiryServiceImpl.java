6bb9327071e36a61ef1f1314a7ad7510a7b3301c
shizit <shizit1995@gmail.com>
2023-07-13 15:28:30 +0900

    dao, serviceの実装

--- src/main/java/com/example/demo/service/InquiryServiceImpl.java
+++ src/main/java/com/example/demo/service/InquiryServiceImpl.java
@@ -2,40 +2,29 @@ package com.example.demo.service;
 
 import java.util.List;
 
-import com.example.demo.entity.Inquiry;
-import com.example.demo.repository.InquiryDao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
-/*
- * Add an annotation here
- */
+import com.example.demo.entity.Inquiry;
+import com.example.demo.dao.InquiryDao;
+
+@Service
 public class InquiryServiceImpl implements InquiryService {
 
 	private final InquiryDao dao;
 
+	@Autowired
 	public InquiryServiceImpl(InquiryDao dao) {
 		this.dao = dao;
 	}
 
 	@Override
 	public void save(Inquiry inquiry) {
-		//hands-on
+		dao.insertInquiry(inquiry);
 	}
-
-//  This method is used in the latter chapter
-//	@Override
-//	public void update(Inquiry inquiry) {
-//
-//		//return dao.updateInquiry(inquiry);
-//		if(dao.updateInquiry(inquiry) == 0) {
-//			throw new InquiryNotFoundException("can't find the same ID");
-//		}
-//	}
 
 	@Override
 	public List<Inquiry> getAll() {
-
-		//hands-on
-
-		return null;
+		return dao.getAll();
 	}
 }
